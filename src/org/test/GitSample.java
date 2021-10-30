afa1540bab81b0e363fac295659144207ab2069a
gvigneshkumar <vigneshkumargopu@gmail.com>
2021-10-30 23:08:05 +0530

    my task done

---
+++ src/org/test/GitSample.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class GitSample {
+	private void git1() {
+		System.out.println("hai frds");
+	}
+	public static void main(String[] args) {
+		GitSample j=new GitSample();
+		j.git1();
+	}
+}
